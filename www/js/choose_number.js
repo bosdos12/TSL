cf2f008558465038ecb3a45fcc9b2288dfa5cd12
akzo <anarkazimov98@hotmail.com>
2016-10-22 19:52:32 +0300

    still working on choosing number pages

--- www/js/choose_number.js
+++ www/js/choose_number.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
+    
+  //Learn the number when "equality" button is pressed
+  $("#learn_btn").click(function(){
+    if(typeof hundreds == 'undefined'){hundreds=0;}
+    if(typeof tens == 'undefined'){tens=0;}
+    if(typeof ones == 'undefined'){ones=0;}
+    alert(hundreds + "" + tens + "" + ones);    
+    
+  });
 
   //read value of hundreds and increase its value by one
   $(".huns_up").click(function(){
     hundreds = parseInt($(".number_block .huns").html());
     if(hundreds<9){
-      $(".number_block .huns").html(hundreds + 1)
+      hundreds+=1;
+      $(".number_block .huns").html(hundreds)
     }  
   });
     
@@ -12,15 +22,17 @@ $(document).ready(function(){
   $(".tens_up").click(function(){
     tens = parseInt($(".number_block .tens").html());
     if(tens<9){
-      $(".number_block .tens").html(tens + 1)
+      tens+=1;
+      $(".number_block .tens").html(tens)
     }  
   });
     
   //read value of ones and increase its value by one
   $(".ones_up").click(function(){
-    tens = parseInt($(".number_block .ones").html());
-    if(tens<9){
-      $(".number_block .ones").html(tens + 1)
+    ones = parseInt($(".number_block .ones").html());
+    if(ones<9){
+      ones+=1;
+      $(".number_block .ones").html(ones)
     }  
   });  
     
@@ -28,7 +40,8 @@ $(document).ready(function(){
   $(".huns_down").click(function(){
     hundreds = parseInt($(".number_block .huns").html());
     if(hundreds>0){
-      $(".number_block .huns").html(hundreds - 1)
+    hundreds-=1;
+      $(".number_block .huns").html(hundreds)
     }  
   });
     
@@ -36,15 +49,17 @@ $(document).ready(function(){
   $(".tens_down").click(function(){
     tens = parseInt($(".number_block .tens").html());
     if(tens>0){
-      $(".number_block .tens").html(tens - 1)
+      tens-=1;
+      $(".number_block .tens").html(tens)
     }  
   });
     
   //read value of ones and increase its value by one
   $(".ones_down").click(function(){
-    tens = parseInt($(".number_block .ones").html());
-    if(tens>0){
-      $(".number_block .ones").html(tens - 1)
+    ones = parseInt($(".number_block .ones").html());
+    if(ones>0){
+      ones-=1;
+      $(".number_block .ones").html(ones)
     }  
   });  
  
