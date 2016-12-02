622fae3afdf01c4a064fe77159397af28e7a937c
akzo <anarkazimov98@hotmail.com>
2016-12-02 06:22:23 +0300

    working on colourse section. Added interactive images to the majority of colours

--- www/js/colours.js
+++ www/js/colours.js
@@ -1,63 +1,118 @@
-$(document).ready(function(){
-   
-  $(".circle").click(function(){
-    $(".circle").removeClass("selected");
-    $(this).addClass("selected");
-      
-    chosen_colour = $(this).css("backgroundColor");
-    setInterval(function(){
-      //While the chosen colour is green show gestures for green
-      if(chosen_colour == "rgb(0, 128, 0)"){
-        $("#colour_gesture").attr("src", "../../images/gestures/colours/green_1.jpg")
-        setTimeout(function(){
-          if(chosen_colour == "rgb(0, 128, 0)"){
-            $("#colour_gesture").attr("src", "../../images/gestures/colours/green_2.jpg") 
-          }
-        }, 1000) 
-        }
+$(document).ready(function(){ 
+    
+  //show gesture for red colour as default
+  show_gesture("rgb(255, 0, 0)")
+  $("#default").addClass("selected");
+    
+  function show_gesture(colour){
+    //Delete unnecessary background intervals
+    try{
+      window.clearInterval(refreshIntervalId);
+    } catch(err){}
+    
+    //this is an interval that checks for chosem circles and update gestures if necessary
+    window.refreshIntervalId = setInterval(function(){
         
-      //While the chosen colour is red show gestures for green
-      else if(chosen_colour == "rgb(255, 0, 0)"){
-        $("#colour_gesture").attr("src", "../../images/gestures/colours/red_1.jpg")
-        setTimeout(function(){
-          if(chosen_colour == "rgb(255, 0, 0)"){
-            $("#colour_gesture").attr("src", "../../images/gestures/colours/red_2.jpg") 
-          }
-        }, 1000)
+      //Red colour  
+      if(colour == "rgb(255, 0, 0)"){
+        current_image = $("#colour_gesture").attr("src");        
+        if(current_image.indexOf("red_1") != -1){
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/red_3.jpg"); 
+        }
+        else{
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/red_1.jpg");
+        }
+          
+        
       }
         
+      //Green colour  
+      else if (colour == "rgb(0, 128, 0)"){
+          
+        current_image = $("#colour_gesture").attr("src");  
+        if(current_image.indexOf("green_1") != -1){ 
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/green_2.jpg");
+        }
+          
+        else{
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/green_1.jpg"); 
+        }
+          ``        
+      }
         
-    }, 3000);
+      //Brown colour  
+      else if (colour == "rgb(165, 42, 42)"){
+          
+        current_image = $("#colour_gesture").attr("src");  
+        if(current_image.indexOf("brown_1") != -1){ 
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/brown_2.jpg");
+        }
+          
+        else{
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/brown_1.jpg"); 
+        }
+          ``        
+      }
         
+     //Black colour  
+      else if (colour == "rgb(0, 0, 0)"){
+          
+        current_image = $("#colour_gesture").attr("src");  
+        if(current_image.indexOf("black_1") != -1){ 
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/black_2.jpg");
+        }
+          
+        else{
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/black_1.jpg"); 
+        }
+          ``        
+      }
+        
+      //White colour  
+      else if (colour == "rgb(255, 255, 255)"){
+          
+        current_image = $("#colour_gesture").attr("src");  
+        if(current_image.indexOf("white_1") != -1){ 
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/white_2.jpg");
+        }
+          
+        else{
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/white_1.jpg"); 
+        }
+          ``        
+      }
+        
+      //Grey colour  
+      else if (colour == "rgb(128, 128, 128)"){
+          
+        current_image = $("#colour_gesture").attr("src");  
+        if(current_image.indexOf("grey_1") != -1){ 
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/grey_2.jpg");
+        }
+          
+        else{
+          current_image = $("#colour_gesture").attr("src", "../../images/gestures/colours/grey_1.jpg"); 
+        }
+          ``        
+      }
+        
+    }, 2000);
       
-    /*
-   
-    else if(chosen_colour == "rgb(0, 0, 255)"){
-      $("#colour_gesture").attr("src", "")
-    }
-    else if(chosen_colour == "rgb(165, 42, 42)"){
-      $("#colour_gesture").attr("src", "../../images/gestures/colours/brown_1.jpg")
-    }
-    else if(chosen_colour == "rgb(0, 0, 0)"){
-      $("#colour_gesture").attr("src", "../../images/gestures/colours/black_1.jpg")
-    }
-    else if(chosen_colour == "rgb(255, 255, 255)"){
-      $("#colour_gesture").attr("src", "../../images/gestures/colours/white_1.jpg")
-    }
-    else if(chosen_colour == "rgb(128, 128, 128)"){
-      $("#colour_gesture").attr("src", "../../images/gestures/colours/grey_1.jpg")
-    }
-    else if(chosen_colour == "rgb(255, 165, 0)"){
-      $("#colour_gesture").attr("src", "")
-    }
-    else if(chosen_colour == "rgb(255, 255, 0)"){
-      $("#colour_gesture").attr("src", "../../images/gestures/colours/yellow_1.jpg")
-    }
-    else if(chosen_colour == "rgb(255, 192, 203)"){
-      $("#colour_gesture").attr("src", "../../images/gestures/colours/pink_1.jpg")
-    }*/
+      
+      
+  }
+    
+  //$(".circle").click(show_gesture(this));
+  
+  //Colours chosen after default was shown
+  $(".circle").click(function(){
+    $(".circle").removeClass("selected"); //unclicking previously chosen circles
+    $(this).addClass("selected"); 
+    chosen_colour = $(".selected").css("background-color");
+    show_gesture(chosen_colour)
+    
+    alert(chosen_colour);
   });
-    
-    
-
+                     
+  
 });
