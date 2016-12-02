ab17d232b6673d847dce539d5fd46303a881f264
akzo <anarkazimov98@hotmail.com>
2016-12-02 23:20:44 +0300

    changed the greeting section

--- www/js/greeting.js
+++ www/js/greeting.js
@@ -7,9 +7,11 @@ $(document).ready(function(){
   jQuery('.2gstr_sign').each(function () {
       if(first_gesture==1){
         jQuery(this).attr('src', jQuery(this).attr('src').replace("1", "2"));
+        $(".order").html("2");
       }
       else if (first_gesture == 0){
         jQuery(this).attr('src', jQuery(this).attr('src').replace("2", "1"));
+        $(".order").html("1");
       }
     });
     first_gesture = 1 - first_gesture; 
@@ -18,14 +20,20 @@ setInterval(two_gstr_signs, 1500);
 
  
  function three_gstr_signs(){
+    $(".3_order").html("1");
+    setTimeout(function() {  $(".3_order").html("2"); }, 500);
+    setTimeout(function() {  $(".3_order").html("3"); }, 2000);
+    setTimeout(function() {  $(".3_order").html("1"); }, 3500);
+     
    //how are you
    $("#h_r_u").attr("src", "../../images/gestures/greeting/h_r_u_1.jpg");
    setTimeout(function() { $("#h_r_u").attr("src", "../../images/gestures/greeting/h_r_u_2.jpg"); }, 1500)
    setTimeout(function() { $("#h_r_u").attr("src", "../../images/gestures/greeting/point_to.jpg"); }, 3000)
    
    //I am fine
+   
    $("#fine").attr("src", "../../images/gestures/greeting/fine_1.jpg");
-   setTimeout(function() { $("#fine").attr("src", "../../images/gestures/greeting/fine_2.jpg"); }, 1500)
+   setTimeout(function() {$("#fine").attr("src", "../../images/gestures/greeting/fine_2.jpg"); }, 1500)
    setTimeout(function() { $("#fine").attr("src", "../../images/gestures/greeting/self_point.jpg"); }, 3000)
    
    //What is your name
